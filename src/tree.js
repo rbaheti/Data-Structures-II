d681368c5b7f0602e85af7cc4f4233bfdb222ef9
Rashmi Baheti <baheti.rash123@gmail.com>
2017-10-12 12:06:09 -0700

    Completerd Tree.js
    Rashmi, Mikias - CS4

--- src/tree.js
+++ src/tree.js
@@ -16,7 +16,13 @@ class Tree {
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    if (this.value === value) {
+      return true;
+    }
+    for (let i = 0; i < this.children.length; ++i) {
+      if (this.children[i].contains(value)) return true;
+    }
+    return false;
   }
 }
 
